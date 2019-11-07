6976970f74e13363e6f61ebbd0f58eb124531579
Carlos Martínez <cmartinez@cipf.es>
2019-11-07 10:56:41 +0100

    Updated organism configuration to reflect latest biomart changes.

--- PaintomicsServer/src/conf/organismDB.py
+++ PaintomicsServer/src/conf/organismDB.py
@@ -1,7 +1,8 @@
 dicDatabases = {
-        'mmu'   :   [{'KEGG': 'entrezgene'}, {'KEGG': 'refseq_gene_symbol'}],
+        'mmu'   :   [{'KEGG': 'entrezgene', 'Reactome': 'reactome_gene_id'}, {'KEGG': 'refseq_gene_symbol', 'Reactome': 'reactome_gene_id'}],
         'hsa'   :   [{'KEGG': 'entrezgene', 'Reactome': 'reactome_gene_id'}, {'KEGG': 'refseq_gene_symbol', 'Reactome': 'reactome_gene_id'}],
         'dosa'  :   [{'KEGG': 'ensembl_transcript'}, {'KEGG': 'kegg_gene_symbol'}],
+        'sly'   :   [{'KEGG': 'entrezgene'}, {'KEGG': 'kegg_gene_symbol'}],
         'rno'   :   [{'KEGG': 'entrezgene'}, {'KEGG': 'refseq_gene_symbol'}],
         'sot'   :   [{'KEGG': 'kegg_id', 'MapMan': 'mapman_gene_id'}, {'KEGG': 'kegg_gene_symbol', 'MapMan': 'kegg_gene_symbol'}]
     }
